9c86572a8be030080e3b93febd94abbf7365b5ef
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-06-15 17:49:46 +0100

    - Fixed seed data references

--- app/backend/src/database/migrations/development/20190507161717_init_empty_database.js
+++ app/backend/src/database/migrations/development/20190507161717_init_empty_database.js
@@ -1,9 +1,15 @@
 const {
-    Users
+    Users,
+    AddressCountries,
+    AddressCities,
+    AddressLocations,
+    Addresses,
+    MailSenders,
+    MailSenderNames
 } = require('../../models');
 
 exports.up = async knex => {
-    await knex.schema.createTable('users', table => {
+    await knex.schema.createTable(Users.tableName, table => {
         table.increments('id').primary();
         table.string('email');
         table.string('password');
@@ -12,55 +18,55 @@ exports.up = async knex => {
         table.timestamps();
     });
 
-    await knex.schema.createTable('address_countries', table => {
+    await knex.schema.createTable(AddressCountries.tableName, table => {
         table.increments('id').primary();
         table.string('name').notNullable();
         table.string('short_name').notNullable();
         table.unique(['name', 'short_name']);
     });
 
-    await knex.schema.createTable('address_cities', table => {
+    await knex.schema.createTable(AddressCities.tableName, table => {
         table.increments('id').primary();
         table.string('name').notNullable();
-        table.integer('country_id').references('id').inTable('address_countries').notNullable();
+        table.integer('country_id').references('id').inTable(AddressCountries.tableName).notNullable();
         table.unique(['name', 'country_id']);
     });
 
-    await knex.schema.createTable('address_locations', table => {
+    await knex.schema.createTable(AddressLocations.tableName, table => {
         table.increments('id').primary();
         table.string('postcode').notNullable();
-        table.integer('city_id').references('id').inTable('address_cities').notNullable();
+        table.integer('city_id').references('id').inTable(AddressCities.tableName).notNullable();
         table.unique(['postcode', 'city_id']);
     });
 
-    await knex.schema.createTable('addresses', table => {
+    await knex.schema.createTable(Addresses.tableName, table => {
         table.increments('id').primary();
-        table.integer('location_id').references('id').inTable('address_locations');
+        table.integer('location_id').references('id').inTable(AddressLocations.tableName);
         table.string('address_line_1');
         table.string('address_line_2');
         table.timestamps();
     });
 
-    await knex.schema.createTable('mail_sender_names', table => {
+    await knex.schema.createTable(MailSenderNames.tableName, table => {
         table.increments('id').primary();
         table.string('name');
         table.unique(['name']);
     });
 
-    await knex.schema.createTable('mail_senders', table => {
+    await knex.schema.createTable(MailSenders.tableName, table => {
         table.increments('id').primary();
-        table.integer('address_id').references('id').inTable('addresses');
-        table.integer('name_id').references('id').inTable('mail_sender_names');
+        table.integer('address_id').references('id').inTable(Addresses.tableName);
+        table.integer('name_id').references('id').inTable(MailSenderNames.tableName);
         table.unique(['address_id', 'name_id']);
     });
 };
 
 exports.down = async knex => {
-    await knex.schema.dropTableIfExists('users');
-    await knex.schema.dropTableIfExists('mail_senders');
-    await knex.schema.dropTableIfExists('mail_sender_names');
-    await knex.schema.dropTableIfExists('addresses');
-    await knex.schema.dropTableIfExists('address_locations');
-    await knex.schema.dropTableIfExists('address_cities');
-    await knex.schema.dropTableIfExists('address_countries');
+    await knex.schema.dropTableIfExists(Users.tableName);
+    await knex.schema.dropTableIfExists(MailSenders.tableName);
+    await knex.schema.dropTableIfExists(MailSenderNames.tableName);
+    await knex.schema.dropTableIfExists(Addresses.tableName);
+    await knex.schema.dropTableIfExists(AddressLocations.tableName);
+    await knex.schema.dropTableIfExists(AddressCities.tableName);
+    await knex.schema.dropTableIfExists(AddressCountries.tableName);
 };
